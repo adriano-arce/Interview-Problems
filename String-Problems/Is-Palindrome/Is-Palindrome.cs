fe72d461ac61f834e3c1b19065594159abd0bc1f
Adriano Arce <Adriano.Arce@sage.com>
2014-10-04 15:45:05 -0700

    Implemented a hacky ToRepr function for C# that mimics Python's repr function.

--- String-Problems/Is-Palindrome/Is-Palindrome.cs
+++ String-Problems/Is-Palindrome/Is-Palindrome.cs
@@ -3,15 +3,20 @@ using System.Linq;
 
 public class IsPalindromeSolution
 {
-    // Returns true iff s is a palindrome.
-    // Clarification to ask the interviewer:
-    // - Are whitespace, punctuation, and case considered important?
-    // - For this function, we assume that they are NOT important.
+    /// <summary>
+    /// Returns true iff s is a palindrome.
+    /// Clarification to ask the interviewer:
+    /// - Are whitespace, punctuation, and case considered important?
+    /// - For this function, we assume that they are NOT important.
+    /// </summary>
+    /// <param name="s">The candidate palindrome string.</param>
+    /// <returns>True iff s is a palindrome.</returns>
     public static bool IsPalindrome(string s)
     {
         // Strip out all whitespace and punctuation, and ignore case.
         var strippedChars = s.ToCharArray()
-                             .Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                             .Where(c => !char.IsWhiteSpace(c)
+                                         && !char.IsPunctuation(c))
                              .ToArray();
         var stripped = new string(strippedChars).ToLower();
 
@@ -26,6 +31,18 @@ public class IsPalindromeSolution
         }
         return true;
     }
+    
+    /// <summary>
+    /// Returns the raw representation of s. Surrounds s with single quotes
+    /// and allows us to see newlines and tabs.
+    /// </summary>
+    /// <param name="s">The string to be processed.</param>
+    /// <returns>The raw representation of s.</returns>
+    public static string ToRepr(string s)
+    {
+        return "'" + s.Replace("\n", @"\n").Replace("\t", @"\t") + "'";
+    }
+
     public static void Main()
     {
         var testStrings = new[] {
@@ -44,12 +61,12 @@ public class IsPalindromeSolution
         foreach (var s in testStrings)
         {
             Console.WriteLine("Test String #{0}:", testCase);
-            Console.WriteLine(IsPalindrome(s) ? "Palindrome." : "Not a palindrome.");
+            Console.WriteLine("{0} {1} a palindrome.",
+                              ToRepr(s), IsPalindrome(s) ? "is" : "is not");
             testCase++;
         }
 
-        Console.WriteLine();
-        Console.WriteLine("Press any key to continue...");
+        Console.Write("\nPress any key to continue...");
         Console.ReadKey();
     }
 }
