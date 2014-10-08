da01ade00adb7ffb4842503cc57703600774368e
Adriano Arce <Adriano.Arce@sage.com>
2014-10-08 15:34:06 -0700

    Added an Is Rotation string problem with an elegant C# solution. - TODO: Implement a naive solution.

--- CSharp-Solutions/Is-Rotation-Solution/Properties/AssemblyInfo.cs
+++ CSharp-Solutions/Is-Rotation-Solution/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rotate-Matrix-Solution")]
+[assembly: AssemblyTitle("Is-Rotation-Solution")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rotate-Matrix-Solution")]
+[assembly: AssemblyProduct("Is-Rotation-Solution")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee15fa35-e491-406e-b106-dc03f5b0423f")]
+[assembly: Guid("0198788f-04bd-428d-ae9f-0556b7bf6be7")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ String-Problems/Is-Rotation/Is-Rotation.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+
+public class IsRotationSolution
+{
+    public static bool ElegantIsRotation(string s, string t)
+    {
+        return s.Length == t.Length && (s + s).Contains(t);
+    }
+    public static bool NaiveIsRotation(string s, string t)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// The entry point.
+    /// </summary>
+    public static void Main()
+    {
+        var testStringPairs = new[]
+        {
+			new[] {"stackoverflow", "flowstackover"},
+            new[] {"animal", "alanim"},
+            new[] {"stackoverflow", "flowstackovez"},
+            new[] {"stackoverflow", "flowstackove"}
+		};
+        var testCase = 1;
+        foreach (var stringPair in testStringPairs)
+        {
+            var s = stringPair[0];
+            var t = stringPair[1];
+            Console.WriteLine("Test String Pair #{0}:", testCase);
+            Console.WriteLine("({0}, {1}) {2} rotations of each other.",
+                              s, t, ElegantIsRotation(s, t) ? "are" : "are not");
+            testCase++;
+        }
+
+        Console.Write("\nPress any key to continue...");
+        Console.ReadKey();
+    }
+}
