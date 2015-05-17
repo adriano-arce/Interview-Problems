dc3180e621a57b06fdf3385142b20c79162ec9a9
Adriano Arce <adriano.arce@hotmail.ca>
2015-05-17 00:46:10 -0700

    Solved the Is Balanced problem in Python.

---
+++ String-Problems/Is-Balanced/Is-Balanced.py
@@ -0,0 +1,32 @@
+def is_balanced(s):
+    """
+    Returns True iff all types of brackets in s are balanced.
+    """
+    bracket_dict = {"(": ")", "[": "]", "{": "}"}
+    right_bracket_set = {")", "]", "}"}
+    right_bracket_stack = []
+    for char in s:
+        # If char is a left bracket, then add it to the stack.
+        right_bracket = bracket_dict.get(char)
+        if right_bracket:
+            right_bracket_stack.append(right_bracket)
+        # If char is a right bracket, then try popping it from the stack.
+        elif char in right_bracket_set:
+            if right_bracket_stack and char == right_bracket_stack[-1]:
+                right_bracket_stack.pop()
+            else:
+                return False
+    return not right_bracket_stack
+
+
+if __name__ == "__main__":
+    testStrings = [
+        "(2 + y) - e^{2x + 3} * [7x] / [sin(2e^{7x})]",
+        "5(2 + x]",
+        "4)x + 3(",
+        "[[[(({})[])]]{()}]"
+    ]
+    for (index, s) in enumerate(testStrings):
+        print(str.format("Test String #{0}:", index + 1))
+        print(str.format("{0} {1} balanced.",
+                         repr(s), "is" if is_balanced(s) else "is not"))
