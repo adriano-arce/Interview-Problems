054db78bb59057728c74837adcdddfdeaf63ee1a
Adriano Arce <adriano.arce@hotmail.ca>
2015-05-20 00:56:51 -0700

    Solved the Add Two Nums problem in Python using both iteration and recursion.

--- LinkedList-Problems/Add-Two-Nums/Add-Two-Nums.py
+++ LinkedList-Problems/Add-Two-Nums/Add-Two-Nums.py
@@ -4,10 +4,61 @@ class Node:
         self.next_node = next_node
 
 
+def add_recurse(head1, head2, carry):
+    # Base Cases: Both heads are None.
+    if not head1 and not head2:
+        if carry:
+            return Node(carry)
+        return None
+
+    # Recursive Step: Separate into a head and tail.
+    digit1 = head1.data if head1 else 0
+    digit2 = head2.data if head2 else 0
+    next1 = head1.next_node if head1 else None
+    next2 = head2.next_node if head2 else None
+    carry, digit = divmod(digit1 + digit2 + carry, 10)  # Assume base 10.
+    tail = add_recurse(next1, next2, carry)
+    return Node(digit, tail)
+
+
 def add_two_nums(head1, head2):
-    tail = Node(1)
-    head_sum = Node(2, tail)
-    return head_sum
+    """
+    Adds the two given numbers (stored as linked lists).
+    """
+
+    #---------------------------------------------------------------------------
+    # Solution 1: Iteration.
+    #---------------------------------------------------------------------------
+    # head = None
+    # prev, tail = None, head
+    # carry = 0
+    # ptr1, ptr2 = head1, head2
+    # while ptr1 or ptr2:
+    #     # Add the digits and extend the linked list.
+    #     digit1 = ptr1.data if ptr1 else 0
+    #     digit2 = ptr2.data if ptr2 else 0
+    #     carry, digit = divmod(digit1 + digit2 + carry, 10)  # Assume base 10.
+    #     tail = Node(digit)
+    #     if head:  # A previous node exists.
+    #         prev.next_node = tail
+    #     else:  # This is the first node.
+    #         head = tail
+    #
+    #     # Update for the next iteration.
+    #     prev = tail
+    #     if ptr1:
+    #         ptr1 = ptr1.next_node
+    #     if ptr2:
+    #         ptr2 = ptr2.next_node
+    # if carry:
+    #     tail = Node(carry)
+    #     prev.next_node = tail  # prev must exist if carry is nonzero
+    #
+    # return head
+    #---------------------------------------------------------------------------
+    # Solution 2: Recursion.
+    #---------------------------------------------------------------------------
+    return add_recurse(head1, head2, 0)
 
 
 def print_num(head):
@@ -16,28 +67,24 @@ def print_num(head):
     while ptr:
         digits.append(ptr.data)
         ptr = ptr.next_node
-    print(digits)
     print("".join(map(str, reversed(digits))))
 
+
+def python_2_linked(python_list):
+    head = None
+    for i in range(len(python_list) - 1, -1, -1):
+        head = Node(python_list[i], head)
+    return head
+
+
 if __name__ == "__main__":
-    testHeadPairs = []
-
-    case1node5 = Node(5)
-    case1node4 = Node(4, case1node5)
-    case1node3 = Node(3, case1node4)
-    case1head2 = Node(2, case1node3)
-    case1head1 = Node(1, case1head2)
-    testHeadPairs.append((case1head1, case1head2))
-
-    case2node1 = Node(1, case1node3)
-    case2head1 = Node(0, case2node1)
-    testHeadPairs.append((case2head1, case1head2))
-
-    case3node5 = Node(5)
-    case3node4 = Node(4, case3node5)
-    case3node3 = Node(3, case3node4)
-    case3head1 = Node(2, case3node3)
-    testHeadPairs.append((case3head1, case1head2))
+    testHeadPairs = [
+        (python_2_linked([2, 1]), python_2_linked([4, 3])),
+        (python_2_linked([5]), python_2_linked([7])),
+        (python_2_linked([9, 8, 7]), python_2_linked([8, 7, 6])),
+        (python_2_linked([1]), python_2_linked([9, 9, 9, 9])),
+        (python_2_linked([0, 0, 0, 1]), python_2_linked([2]))
+    ]
 
     for (index, headPair) in enumerate(testHeadPairs):
         print("Test Pair #{}:".format(index + 1))
