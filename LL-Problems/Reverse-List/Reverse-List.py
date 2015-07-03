7830fa1b69c51facf22bd773e95a93daf3e19f11
Adriano Arce <adriano.arce@hotmail.ca>
2015-07-03 01:50:12 -0700

    Solved the Reverse-List problem in Python using tail recursion and iteration.

--- LL-Problems/Reverse-List/Reverse-List.py
+++ LL-Problems/Reverse-List/Reverse-List.py
@@ -30,28 +30,49 @@ def reverse_list(head):
     #---------------------------------------------------------------------------
     # Method 1: Recursive.
     #---------------------------------------------------------------------------
-    if head:
-        new_head = reverse_list(head.next_node)
-        # Right now, head.next_node is the tail of the sublist.
-        # We need the sublist's tail to be the (old) head instead.
-        if new_head:
-            head.next_node.next_node = head
-            head.next_node = None
-            return new_head
-        return head  # Linked list is just one node.
-    return None  # Linked list is empty.
+    # if head:
+    #     new_head = reverse_list(head.next_node)
+    #     # Right now, head.next_node is the tail of the sublist.
+    #     # We need the sublist's tail to be the (old) head instead.
+    #     if new_head:
+    #         head.next_node.next_node = head
+    #         head.next_node = None
+    #         return new_head
+    #     return head  # Linked list is just one node.
+    # return None  # Linked list is empty.
 
     #---------------------------------------------------------------------------
     # Method 2: Tail recursive (the recursive call is at the end).
-    # In languages other than Python, the compiler uses tail call optimization.
-    # In the next recursive call, we can simply reuse the current stack frame.
-    # It's also easy to convert this to iterative.
     #---------------------------------------------------------------------------
+    # In languages other than Python, the compiler uses tail call optimization.
+    # So for each recursive call, we can simply reuse the current stack frame.
+    # It's also easy to convert tail recursive to iterative.
+    #---------------------------------------------------------------------------
+    # return tail_recurse(None, head)
 
     #---------------------------------------------------------------------------
     # Method 3: Iterative.
     #---------------------------------------------------------------------------
+    prev, curr = None, head
+    while curr:
+        curr.next_node, prev, curr = prev, curr, curr.next_node
+    return prev
 
+
+def tail_recurse(prev, curr):
+    """
+    Using tail recursion, reverses the linked list starting at curr, then joins
+    the end of this linked list to the linked list starting at prev.
+    """
+    if curr:
+        new_curr = curr.next_node
+        if new_curr:
+            curr.next_node = prev
+            return tail_recurse(curr, new_curr)
+        # We've reached the end.
+        curr.next_node = prev
+        return curr
+    return None  # Linked list is empty/
 
 if __name__ == "__main__":
     testHeads = [
