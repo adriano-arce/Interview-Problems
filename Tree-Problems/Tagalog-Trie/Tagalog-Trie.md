a5105014e800e8665d893caa7cdbcb51215a6c4c
Adriano Arce <adriano.arce@hotmail.ca>
2015-07-06 00:26:42 -0700

    Added and solved the Tagalog-Trie problem in Python.

---
+++ Tree-Problems/Tagalog-Trie/Tagalog-Trie.md
@@ -0,0 +1,6 @@
+Tagalog Trie
+==================
+
+Sorts a given list of Tagalog words.
+
+Source: http://red.cliff.jp/topcoder/TagalogDictionary.txt
---
+++ Tree-Problems/Tagalog-Trie/Tagalog-Trie.py
@@ -0,0 +1,57 @@
+import re
+
+REGEX = re.compile(r"(ng|.)")
+ALPHABET = "a b k d e g h i l m n ng o p r s t u w y".split()
+NUM_CHARS = len(ALPHABET)
+CHAR_MAP = {c: i for i, c in enumerate(ALPHABET)}
+
+
+class Node:
+    def __init__(self, children, is_word=False):
+        self.children = children
+        self.is_word = is_word
+
+
+def insert(word, trie):
+    chars = REGEX.findall(word)
+    for char in chars:
+        index = CHAR_MAP[char]
+        if not trie.children[index]:
+            trie.children[index] = Node([None] * NUM_CHARS)
+        trie = trie.children[index]
+    trie.is_word = True
+
+
+def traverse(trie, prefix):
+    words = []
+    if trie.is_word:
+        words.append(prefix)
+    for i in range(NUM_CHARS):
+        if trie.children[i]:
+            words.extend(traverse(trie.children[i], prefix + ALPHABET[i]))
+    return words
+
+
+def sort_words(words):
+    trie = Node([None] * NUM_CHARS)
+    for word in words:
+        insert(word, trie)
+    return traverse(trie, "")
+
+
+def main():
+    test_lists = [
+        ["abakada", "alpabet", "tagalog", "ako"],
+        ["ang","ano","anim","alak","alam","alab"],
+        ["siya","niya","kaniya","ikaw","ito","iyon"],
+        ["kaba","baka","naba","ngipin","nipin"],
+        ["knilngiggnngginggn","ingkigningg","kingkong","dingdong","dindong","dingdont","ingkblot"],
+        ["silangang", "baka", "bada", "silang"]
+    ]
+    for i, test_list in enumerate(test_lists):
+        print("Test Case #{}:".format(i + 1))
+        print("   BEFORE: {}".format(test_list))
+        print("    AFTER: {}".format(sort_words(test_list)))
+
+if __name__ == "__main__":
+    main()
