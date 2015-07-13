cc574205a5f6da45c28e32bc83b3245d12bce881
Adriano Arce <adriano.arce@hotmail.ca>
2015-07-13 00:02:41 -0700

    Added the Sieve of Eratosthenes approach for the Is-Prime problem.

--- Math-Problems/Is-Prime/Is-Prime.py
+++ Math-Problems/Is-Prime/Is-Prime.py
@@ -1,4 +1,5 @@
 import math
+import sys, os
 
 
 def is_prime(n):
@@ -27,13 +28,11 @@ def is_prime(n):
     #---------------------------------------------------------------------------
     # Method 3: Miller-Rabin.
     #---------------------------------------------------------------------------
-    if n == 2:
-        print("  2 is the only even prime.")
-        return True
-    if n % 2 == 0:
-        print("  {} > 2 is even.".format(n))
-        return False
     bases = [2, 7, 61]  # Works for all n < 4759123141.
+    if n in bases:
+        print("  {} is one of the base primes.".format(n))
+        return True
+
     for b in bases:
         if is_definitely_composite(n, b):
             return False
@@ -63,14 +62,48 @@ def is_definitely_composite(n, b):
     return True
 
 
+def get_all_primes(n):
+    """
+    Returns a list of all primes 2 <= p <= n.
+    """
+    #---------------------------------------------------------------------------
+    # Method 1: Reuse the is_prime() function.
+    #---------------------------------------------------------------------------
+    # primes = []
+    # for i in range(2, n + 1):
+    #     if is_prime(i):
+    #         primes.append(i)
+    # return primes
+
+    #---------------------------------------------------------------------------
+    # Method 2: Use the Sieve of Eratosthenes.
+    #---------------------------------------------------------------------------
+    i_is_prime = [True] * (n + 1)
+    i_is_prime[0], i_is_prime[1] = False, False
+    mid = int(math.sqrt(n))
+    for i in range(2, mid + 1):
+        if i_is_prime[i]:
+            for j in range(i*i, n + 1, i):
+                i_is_prime[j] = False
+    return [i for i in range(n + 1) if i_is_prime[i]]
+
+
 def main():
     test_nums = [
-        2, 12, 25, 91, 97, 2047, 314821, 2 ** 31 - 3, 2 ** 31 - 1
+        2, 12, 25, 61, 91, 97, 2047, 314821, 2 ** 31 - 3, 2 ** 31 - 1
     ]
     for i, n in enumerate(test_nums):
         print("Test Case #{}:".format(i + 1))
         prime = "is" if is_prime(n) else "is not"
         print("    {} {} prime.".format(n, prime))
+
+    n = 229
+    print(get_all_primes(n))
+    with open(os.devnull, "w") as devnull:
+        old_stdout, sys.stdout = sys.stdout, devnull
+        rabin_miller_primes = [i for i in range(2, n + 1) if is_prime(i)]
+    sys.stdout = old_stdout
+    print(rabin_miller_primes)
 
 if __name__ == "__main__":
     main()
