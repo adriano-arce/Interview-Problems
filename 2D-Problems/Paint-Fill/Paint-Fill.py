2c04437c0127dbdefda1776f722a1bda3f97e153
Adriano Arce <adriano.arce@hotmail.ca>
2015-07-27 22:30:24 -0700

    Added a test case for the Paint-Fill problem.

---
+++ 2D-Problems/Paint-Fill/Paint-Fill.py
@@ -0,0 +1,18 @@
+def paint_fill(grid, pos, col):
+    pass
+
+
+def main():
+    test_grids = [
+        [
+            list("xxxxxxxxxxxxxxxxxxxxxxx"),
+            list("xxxxooooooooooooooxxxxx"),
+            list("xxxxooooooooooooooooooo"),
+            list("xxxxoxxxxxxxxxxxxxxxoxx"),
+            list("xxxxoxxxxxxxxxxxxxxxoxx"),
+            list("xxxxoxxxxxxxxxxxxxxxoxx")
+        ]
+    ]
+
+if __name__ == "__main__":
+    main()
