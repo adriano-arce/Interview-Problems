7a22bf4e06c1daa77fc466887c5d5ff77973ce2e
Adriano Arce <adriano.arce@hotmail.ca>
2015-07-31 23:54:07 -0700

    Solved the Paint-Fill problem in Java.

--- 2D-Problems/Paint-Fill/PaintFill.java
+++ 2D-Problems/Paint-Fill/PaintFill.java
@@ -1,24 +1,45 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PaintFill
 {
     public enum Colour
     {
-        Black('k'), Red('r'), Blue('b'), Green('g'), White('w');
+        Black, Red, Blue, Green, White;
+    }
 
-        Colour(final char asChar)
+    public static void paintFill(Colour[][] grid, int x, int y, Colour newCol)
+    {
+        paintFill(grid, x, y, grid[x][y], newCol);
+    }
+
+    public static void paintFill(Colour[][] grid, int x, int y, Colour
+            oldCol, Colour newCol)
+    {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (0 <= x && x < m && 0 <= y && y < n && grid[x][y] == oldCol &&
+                grid[x][y] != newCol)
         {
-            this.asChar = asChar;
-        }
-
-        private final char asChar;
-
-        public char asChar()
-        {
-            return asChar;
+            grid[x][y] = newCol;
+            paintFill(grid, x - 1, y, oldCol, newCol);
+            paintFill(grid, x + 1, y, oldCol, newCol);
+            paintFill(grid, x, y - 1, oldCol, newCol);
+            paintFill(grid, x, y + 1, oldCol, newCol);
         }
     }
 
+
     public static void main(String[] args)
     {
+        Map<Character, Colour> colourMap = new HashMap<>();
+        colourMap.put('k', Colour.Black);
+        colourMap.put('r', Colour.Red);
+        colourMap.put('b', Colour.Blue);
+        colourMap.put('g', Colour.Green);
+        colourMap.put('w', Colour.White);
+
         char[][] charGrid = {
                 "kkkkkkkkwwwkkkkkkkkrrkk".toCharArray(),
                 "kkkkbbbbbbbbbbbbbbkkkkk".toCharArray(),
@@ -33,7 +54,40 @@ public class PaintFill
         Colour[][] originalGrid = new Colour[m][n];
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                originalGrid[i][j] = Colour.valueOf(String
-                        .valueOf(charGrid[i][j]));
+                originalGrid[i][j] = colourMap.get(charGrid[i][j]);
+
+        int[][] testPositions = {
+                {0, 0},
+                {0, 0},
+                {1, 4},
+                {1, 4},
+                {1, 4},
+                {2, 4},
+                {2, 4},
+                {2, 4}
+        };
+        Colour[] testColours = {
+                Colour.Black,
+                Colour.Blue,
+                Colour.Black,
+                Colour.Blue,
+                Colour.Red,
+                Colour.Black,
+                Colour.Blue,
+                Colour.Red
+        };
+        assert testPositions.length == testColours.length;
+        for (int i = 0; i < testPositions.length; i++)
+        {
+            Colour[][] grid = Arrays.copyOf(originalGrid, originalGrid.length);
+            int x = testPositions[i][0];
+            int y = testPositions[i][1];
+            Colour newCol = testColours[i];
+            System.out.printf("Test Case #%d: (%d, %d, %s)\n", i + 1, x, y,
+                    newCol.toString());
+
+            paintFill(grid, x, y, newCol);
+            System.out.println(Arrays.deepToString(grid));
+        }
     }
 }
