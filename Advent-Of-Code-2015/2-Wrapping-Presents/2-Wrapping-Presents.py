60c3e4590c1031001d3c00a98d5c748d6b19498b
Adriano Arce <adriano.arce@hotmail.ca>
2015-12-29 23:55:21 -0800

    Added and solved Day 2 of the Advent of Code in Python.

---
+++ Advent-Of-Code-2015/2-Wrapping-Presents/2-Wrapping-Presents.py
@@ -0,0 +1,19 @@
+def main():
+    with open('input.txt', 'r') as f:
+        rows = f.read().split('\n')
+
+    total_paper = 0
+    total_ribbon = 0
+    for row in rows:
+        l, w, h = (int(d) for d in row.split('x'))
+        faces = l*w, w*h, h*l
+        total_paper += 2*sum(faces) + min(faces)
+
+        sorted_dimensions = sorted((l, w, h))
+        total_ribbon += 2*sum(sorted_dimensions[:2]) + l*w*h
+
+    print('Total required paper: %d square feet.' % total_paper)
+    print('Total required ribbon: %d feet.' % total_ribbon)
+
+if __name__ == '__main__':
+    main()
