ee652c292e71e77a94280feb34f833b5e356a2d6
Adriano Arce <adriano.arce@hotmail.ca>
2016-01-01 23:59:04 -0800

    Added an HTML scraper for Advent of Code.
    This saves me from having to do a lot manual fiddling when copying over
    to the markdown file.

---
+++ Advent-Of-Code-2015/html_scraper.py
@@ -0,0 +1,51 @@
+from pyquery import PyQuery
+from textwrap import wrap
+
+source = 'http://adventofcode.com/day/11'
+d = PyQuery(source)
+
+# print('BEFORE:')
+# print(d('article'))
+
+for tag in ('code', 'em'):
+    for node in d(tag).items():
+        node.replace_with(node.text())
+
+for node in d('p').items():
+    content = node.text()
+    if node.next() and node.next()[0].tag != 'ul':
+        content += '\n'
+    node.replace_with(content)
+
+for node in d('li').items():
+    node.replace_with('- ' + node.text())
+for node in d('ul').items():
+    node.replace_with(node.text() + '\n')
+
+for node in d('h2').items():
+    header = node.text()[4:-4]
+    node.replace_with(header + '\n' + ('=' * len(header)) + '\n\n')
+
+# print('\n\n\n')
+# print('AFTER:')
+# print(d('article'))
+# for c in d('article').children():
+#     print('%s: %s' % (c.tag, c.text))
+
+# print('\n\n\n')
+# print('FINAL:')
+final = []
+margin = 80
+for row in d('article').text().split('\n'):
+    if row == '':
+        final.append(row)
+    elif row.startswith('- '):
+        sub_rows = wrap(row, margin - 2)
+        for i in range(1, len(sub_rows)):
+            sub_rows[i] = '  ' + sub_rows[i]
+        final.extend(sub_rows)
+    else:
+        final.extend(wrap(row, margin))
+
+final.append('\nSource: %s\n' % source)
+print('\n'.join(final))
